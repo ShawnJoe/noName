6c7e459c8b2288af24e415d57b04aac5ca59e384
shawnjoe <shawnjoenine@gmail.com>
2019-01-03 22:56:07 +0800

    修改pagehelper

--- dreamland-web/src/main/java/com/xu/controller/IndexController.java
+++ dreamland-web/src/main/java/com/xu/controller/IndexController.java
@@ -7,28 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
+import com.xu.common.PageHelper;
+import com.xu.common.PageHelper.Page;
+import com.xu.dao.UserContentMapper;
 import com.xu.entity.UserContent;
-import com.xu.service.UserContentService;
 
 @RestController
 @RequestMapping("/index")
 public class IndexController extends BaseController{
 	@Autowired
-	UserContentService userContentServiceImpl;
+	UserContentMapper userContentMapper;
 	
 	@RequestMapping("/init")
 	@ResponseBody
 	public Page indexInit() {
-	    PageHelper.startPage(0, 7);//开始分页
-	    PageHelper.orderBy("id DESC");
-	    List<UserContent> list =  userContentServiceImpl.select(null);
-	    PageInfo<UserContent> info = new PageInfo(list);
-	    System.out.println(list.toString());
-	    Page engPage = new Page();
-	    return engPage;
+		PageHelper.startPage(null, null);//开始分页
+        List<UserContent> list =  userContentMapper.select( null );
+        PageHelper.Page endPage = PageHelper.endPage();//分页结束
+	    return endPage;
 	}
     
 }
